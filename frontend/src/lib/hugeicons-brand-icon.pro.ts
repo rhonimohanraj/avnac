904b9dd666932a1ba8e372cec0622543ba4d21e6
Akinkunmi <akinkunmioye42@gmail.com>
2026-05-05 09:01:14 +0100

    feat(hugeicons): add pro brand icon export

---
+++ frontend/src/lib/hugeicons-brand-icon.pro.ts
@@ -0,0 +1,4 @@
+import type { IconSvgElement } from '@hugeicons/react'
+import { HugeiconsIcon } from '@hugeicons-pro/core-solid-rounded'
+
+export const hugeiconsBrandIcon: IconSvgElement = HugeiconsIcon
